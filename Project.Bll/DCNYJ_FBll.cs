f3a58da5a8d15d961ca78a6ac843d8210c6f9852
Eric <haoge19890522@163.com>
2017-03-27 00:45:09 +0800

    多重耐药菌职能科室填写页面的BLL

---
+++ Project.Bll/DCNYJ_FBll.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Project.Model;
+using Project.Dal;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Project.Bll
+{
+    class DCNYJ_FBll
+    {
+    }
+}
